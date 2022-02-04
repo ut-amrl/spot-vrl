aae5c097115856b5084fd8330c17f419d1ed84ca
Elvin Yang <eyang@cs.utexas.edu>
2022-02-04 17:16:51 -0600

    Add visible_ground_plane_limits poc

--- spot_vrl/homography/transform.py
+++ spot_vrl/homography/transform.py
@@ -159,3 +159,96 @@ def ground_to_image(
     projected_coords = camera_matrix @ camera_coords
     projected_coords /= projected_coords[2]
     return projected_coords[:2]
+
+
+def visible_ground_plane_limits(
+    ground_tform_camera: npt.NDArray[np.float64],
+    camera_matrix: npt.NDArray[np.float64],
+    img_width: int,
+    img_height: int,
+    horizon_dist: float = 2,
+) -> npt.NDArray[np.float64]:
+    """Estimates the visible limits of the ground plane as viewed from a camera.
+
+    Assumes some set of two adjacent corners in the camera image contain the
+    ground plane and the two opposite corners do not contain the ground plane.
+
+    The two remaining ground plane coordinates are calculated by estimating
+    the horizon.
+
+    Args:
+        ground_tform_camera (npt.NDArray[np.float64]): The camera-to-ground
+            affine transform matrix.
+        camera_matrix (npt.NDArray[np.float64]): The camera intrinsic matrix.
+        img_width (int)
+        img_height (int)
+        horizon_dist (float): The distance from the camera to the horizon.
+
+    Returns:
+        npt.NDArray[np.float64]: A 3x4 matrix of ground plane coordinates.
+    """
+    ground_plane_limits = np.empty((3, 0))
+
+    # fmt: off
+    image_limits = np.array([
+        [0, img_width - 1,              0, img_width - 1],
+        [0,             0, img_height - 1, img_height - 1]
+    ])
+    # fmt: on
+    image_limits_in_plane = image_to_ground(
+        image_limits, ground_tform_camera, camera_matrix
+    )
+
+    for col in image_limits_in_plane.transpose():
+        col = col.reshape((3, 1))
+        if not np.isnan(col).any():
+            ground_plane_limits = np.hstack((ground_plane_limits, col))
+    assert ground_plane_limits.shape == (3, 2)
+
+    # Estimate the endpoints of the horizon in the image by fixing a point
+    # `horizon_dist` directly in front of the camera and finding where the
+    # orthogonal vectors intersect with the boundary of the image.
+    #
+    # This approach is invariant to the orientation of the camera.
+
+    # Project the camera's z axis and location onto the ground plane.
+    ground_rot_camera = ground_tform_camera[:3, :3]
+    camera_dir = np.array([0, 0, 1])
+    camera_dir = ground_rot_camera @ camera_dir
+    camera_dir[2] = 0
+    camera_dir /= np.linalg.norm(camera_dir)
+
+    camera_loc = np.copy(ground_tform_camera[:3, 3])
+    camera_loc[2] = 0
+
+    horizon_point = camera_loc + horizon_dist * camera_dir
+    orthogonal = np.array([-camera_dir[1], camera_dir[0], 0])
+
+    def out_of_image(x: float, y: float) -> bool:
+        return x < 0 or x > img_width - 1 or y < 0 or y > img_height - 1
+
+    camera_tform_ground = np.linalg.inv(ground_tform_camera)
+    for _ in range(2):
+        # binary search for the ground plane distance to the image boundary
+        # intersection
+        EPSILON = 1e-4
+        low = 0.0
+        high = 999.0
+
+        while (high - low > EPSILON):
+            mid = (low + high) * 0.5
+
+            ground_coord = horizon_point + mid * orthogonal
+            image_coord = ground_to_image(ground_coord, camera_tform_ground, camera_matrix)
+            if (out_of_image(image_coord[0], image_coord[1])):
+                high = mid - EPSILON
+            else:
+                low = mid
+
+        intersection = (horizon_point + low * orthogonal).reshape((3, 1))
+        ground_plane_limits = np.hstack((ground_plane_limits, intersection))
+
+        orthogonal = -orthogonal
+
+    assert ground_plane_limits.shape == (3, 4)
+    return ground_plane_limits
