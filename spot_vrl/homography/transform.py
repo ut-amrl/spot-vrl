a518c26cd616af725a39af83574ca6c6d680217d
Elvin Yang <eyang@cs.utexas.edu>
2022-02-04 15:41:25 -0600

    Add ground_to_image poc

--- spot_vrl/homography/transform.py
+++ spot_vrl/homography/transform.py
@@ -31,8 +31,8 @@ def affine3d(
     return tform
 
 
-def as_homogeneous(coords: npt.NDArray[np.float64]) -> npt.NDArray[np.float64]:
-    """Turns column vectors into homogeneous coordinates.
+def to_homogenous(coords: npt.NDArray[np.float64]) -> npt.NDArray[np.float64]:
+    """Converts regular coordinates into homogeneous coordinates.
 
     Args:
         coords (npt.NDArray[np.float64]): A matrix of column vectors.
@@ -47,6 +47,22 @@ def as_homogeneous(coords: npt.NDArray[np.float64]) -> npt.NDArray[np.float64]:
     assert coords.ndim == 2
     _, cols = coords.shape
     return np.vstack((coords, np.ones(cols, dtype=np.float64)))
+
+
+def from_homogeneous(coords: npt.NDArray[np.float64]) -> npt.NDArray[np.float64]:
+    """Converts homogeneous coordinates to regular coordinates.
+
+    Args:
+        coords (npt.NDArray[np.float64]): A matrix of column vectors.
+
+    Returns:
+        npt.NDArray[np.float64]: The input matrix without its last row.
+    """
+    if coords.ndim == 1:
+        coords = coords.reshape(coords.shape[0], 1)
+
+    assert coords.ndim == 2
+    return coords[:-1]
 
 
 def camera_rays(
@@ -70,7 +86,7 @@ def camera_rays(
     assert image_coords.shape[0] == 2
 
     tform_rot = world_tform_camera[:3, :3]
-    rays = tform_rot @ np.linalg.inv(camera_matrix) @ as_homogeneous(image_coords)
+    rays = tform_rot @ np.linalg.inv(camera_matrix) @ to_homogenous(image_coords)
     rays /= np.linalg.norm(rays, axis=0)
 
     return rays
@@ -118,3 +134,28 @@ def image_to_ground(
             rays[:, i] = camera_loc + k * ray
 
     return rays
+
+
+def ground_to_image(
+    ground_coords: npt.NDArray[np.float64],
+    camera_tform_ground: npt.NDArray[np.float64],
+    camera_matrix: npt.NDArray[np.float64],
+) -> npt.NDArray[np.float64]:
+    """Calculates the projected image coordinates of ground plane coordinates.
+
+    Args:
+        ground_coords (npt.NDArray[np.float64]): A 3xN matrix of ground plane
+            coordinates.
+        camera_tform_ground (npt.NDArray[np.float64]): The ground-to-camera
+            affine transform matrix.
+        camera_matrix (npt.NDArray[np.float64]): The camera intrinsic matrix.
+
+    Returns:
+        npt.NDArray[np.float64]: A 2xN matrix of image coordinates.
+    """
+    assert ground_coords.shape[0] == 3
+
+    camera_coords = from_homogeneous(camera_tform_ground @ to_homogenous(ground_coords))
+    projected_coords = camera_matrix @ camera_coords
+    projected_coords /= projected_coords[2]
+    return projected_coords[:2]
