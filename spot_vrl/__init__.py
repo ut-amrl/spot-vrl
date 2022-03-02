5d812889c75ee878321faa6ad8ee616196fa3f9f
Elvin Yang <eyang@cs.utexas.edu>
2022-03-01 21:30:09 -0600

    Limit OMP_NUM_THREADS at the package level

--- spot_vrl/__init__.py
+++ spot_vrl/__init__.py
@@ -1,1 +1,22 @@
-__version__ = '0.1.0'
+import os
+import multiprocessing
+import typing
+
+
+__version__ = "0.1.0"
+
+
+def _get_omp_num_threads() -> int:
+    count: typing.Union[int, str] = os.environ["OMP_NUM_THREADS"]
+    if not count:
+        count = multiprocessing.cpu_count()
+    return int(count)
+
+
+def _set_omp_num_threads(count: int) -> None:
+    os.environ["OMP_NUM_THREADS"] = str(count)
+
+
+# On machines with very high core counts, parallelism exploited by libraries can
+# slow programs down due to shared state / communication overhead.
+_set_omp_num_threads(min(8, _get_omp_num_threads()))
