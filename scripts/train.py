9b8b657fad2281d59223ad946e560364c1381e76
Haresh Karnan <haresh.miriyala@gmail.com>
2022-04-12 14:44:51 -0500

    using many forward passes of visual encoder

--- scripts/train.py
+++ scripts/train.py
@@ -96,10 +96,10 @@ class MyDataLoader(pl.LightningDataModule):
 		print('Val dataset size:', len(self.val_dataset))
 
 	def train_dataloader(self):
-		return DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True)
+		return DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True, num_workers=4)
 
 	def val_dataloader(self):
-		return DataLoader(self.val_dataset, batch_size=self.batch_size, shuffle=True)
+		return DataLoader(self.val_dataset, batch_size=self.batch_size, shuffle=False, num_workers=4)
 
 
 class DualAEModel(pl.LightningModule):
